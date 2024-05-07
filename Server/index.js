e4b46ff61a3ef598e3d62a08e78caefec1fc1802
Severin84 <84severin@gmail.com>
2024-05-07 23:46:00 +0530

    re

--- Server/index.js
+++ Server/index.js
@@ -45,6 +45,7 @@ app.use("/api/database",DataBase)
 app.use('/api/stripe',Stripe)
 const PORT = process.env.PORT || 5000
 //const PORT=5000
+
 app.listen(PORT, () => {  console.log(`Server is running on port ${PORT}`)});
 
 //app.use("/api/admin",AdminRoute)
